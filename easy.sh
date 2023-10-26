7b129cac1b33aa9ea8b71396ceb52e653eb6ec8c
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-26 03:07:44 +0000

    Update easy.sh

--- easy.sh
+++ easy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 clear
+#COLOUR
 red='\e[1;31m'
 yellow='\e[0;33m'
 Blue='\e[1;34m'
 Reset='\e[0;0m'
-title="SF Easy"
+title="SF E-LAUNCH"
 echo -e '\033]2;'$title'\007'
 echo -e '\e[0m\e[3;36m-------------------------------
 \e[3;31m SpiderFoot Easy \e[3;36m
@@ -16,7 +17,7 @@ echo -e '\e[0m\e[3;36m-------------------------------
        CTRL + C To Exit \e[3;36m
 -------------------------------'
 
-echo -e $Blue" ┌─["$red"SF$Blue]──[$red~$Blue]─["$yellow"Easy"$Blue]"
+echo -e $Blue" ┌─["$red"SF$Blue]──[$red~$Blue]─["$yellow"Easy$Blue]"
 read -p      " └─────► " n
 case $n in
     1) clear&&echo "launching spiderfoot on localhost"&&cd&&cd spiderfoot&&python3 sf.py -l 127.0.0.1:5001;;
@@ -24,5 +25,3 @@ case $n in
     3) cd&&clear&&echo "make sure to make backups of your config/keys"&&read -p "Have You Made Backups? Press Enter To Continue"&&sudo rm -r spiderfoot&&git clone https://github.com/smicallef/spiderfoot.git&&cd spiderfoot&&pip3 install -r requirements.txt&&firefox "https://www.spiderfoot.net/documentation/";;
     
 esac
-
- 
