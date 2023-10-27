2c99e4fdab8cf250f12bfa717a9ee0f7b64e1444
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-27 11:48:13 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tor
+
+# Tor identity
+newnym() {
+  echo -n "AUTHENTICATE" | nc 127.0.0.1 9051
+  echo -n "SIGNAL NEWNYM" | nc 127.0.0.1 9051
+}
+
+# Download the repo
+git clone https://github.com/CPScript/Sfoot-Easy
+
+# Change your current directory to the one where you cloned the repository.
+cd Sfoot-Easy/Easy
+
+source easy.sh
