5b7d05784081959137aab4b86dddd90c3df99ad8
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-27 12:40:00 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
-#SIMPLE TOR LAUNCHER | for launching tor befor a repo starts
+#SIMPLE TOR LAUNCHER | for launching tor before a repo starts
 
-clear
-echo PLEASE use a vpn or run this script of a disposible USB or VM!
-sleep 6
-clear
-echo Launching tor before running script, please wait...
-sleep 2
-clear
+# Are you root check
+if [[ $EUID -ne 0 ]]; then
+  echo You are root!
+  sleep 1
+  clear
 
-tor
+  echo PLEASE use a vpn or run this script of a disposible USB or VM!
+  sleep 6
+  clear
+  echo Launching tor before running script, please wait...
+  sleep 2
+  clear
 
-# Tor identity
-newnym() {
-  echo -n "AUTHENTICATE" | nc 127.0.0.1 9051
-  echo -n "SIGNAL NEWNYM" | nc 127.0.0.1 9051
-}
+  tor
 
-# Download the repo
-git clone https://github.com/CPScript/Sfoot-Easy
+  # Tor identity
+  newnym() {
+    echo -n "AUTHENTICATE" | nc 127.0.0.1 9051
+    echo -n "SIGNAL NEWNYM" | nc 127.0.0.1 9051
+  }
 
-# Change Dir
-cd Sfoot-Easy/script
+  # Download the repo
+  git clone https://github.com/CPScript/Sfoot-Easy
 
-# Launch script
-source easy.sh
+  # Change Dir
+  cd Sfoot-Easy/script
+
+  # Launch script
+  source easy.sh
+
+else
+  # Not rooted message | How to root
+  echo Please root!
+  sleep 2
+  echo | Example on how to root your terminal:
+  echo | > sudo su < might require a password >
+fi
